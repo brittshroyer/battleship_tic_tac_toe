5e959c611f4eb47a64a701a512a512259a502373
brittshroyer <britt.shroyer@gmail.com>
2016-12-13 23:30:27 -0600

    scoreboard and shipsails

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+$( document ).ready(function() {
+
 var player1;
 var player2;
 var count1 = 0;
@@ -11,16 +13,16 @@ var turns = 0;
 
 
 
+
 var switchPlayer = function(){
   if(currentPlayer === "X"){
     currentPlayer = "O";
     $('#currentPlayer').text("2");
   }else{
     currentPlayer = "X";
-    $('currentPlayer').text("1");
+    $('#currentPlayer').text("1");
   }
 }
-
 
 var checkBox = function(boxValue){
   if(boxValue !== ""){
@@ -30,12 +32,8 @@ var checkBox = function(boxValue){
   }
 }
 
-
-
-
 var checkWinner = function(){
   var y = currentPlayer;
-
   var turd = (($('#1').text() === y) && ($('#2').text() === y) && ($('#3').text() === y))
   || (($('#4').text() === y) && ($('#5').text() === y) && ($('#6').text() === y))
   || (($('#7').text() === y) && ($('#8').text() === y) && ($('#9').text() === y))
@@ -44,27 +42,13 @@ var checkWinner = function(){
   || (($('#3').text() === y) && ($('#6').text() === y) && ($('#9').text() === y))
   || (($('#1').text() === y) && ($('#5').text() === y) && ($('#9').text() === y))
   || (($('#7').text() === y) && ($('#5').text() === y) && ($('#3').text() === y))
-
   return turd;
 }
-
-//We could turn the turd above into a function:
-
-// function isWinner(id1, id2, id3) {
-//   return (($(id1).text() === y) && ($(id2).text() === y) && ($(id3).text() === y))
-
-
-//and pass the values in like so:
-
-// isWinner('#1', '#2', '#3')
 
 var reset = function(){
   $('.square').text("");
   turns = 0;
 }
-
-
-
 
 
 $('.square').on('click', function(){
@@ -77,45 +61,28 @@ $('.square').on('click', function(){
       turns++;
   }
 
-
-
   if(checkWinner()){
-    $('h3').text(currentPlayer + " Wins!");
+    $('#outcome').text(currentPlayer + " Wins!");
+    $("#outcome").fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200);
     if(currentPlayer === "X"){
       count1++;
-      $('#p1score').text(count1);
+      $('#score1').text(count1);
       reset();
     }else{
       count2++;
-      $('#p2score').text(count2);
+      $('#score2').text(count2);
       reset();
     }
   }else{
     if(turns >= 9){
-      $('h3').text("Tie game!");
+
+      $('#outcome').text("Tie game!");
+      $("#outcome").fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200);
       reset();
     }else{
       switchPlayer();
     }
-
-
   }
-
 });
 
-
-
-
-
-// on each click-check for:
-// 1. which player is this?
-//
-// 2. onclick, is the box full?
-//   -if yes--do not fill
-//   -if no -- fill it
-//
-// 3.is there a winner?
-//   -if yes--alert winner, update score & reset board
-//   -if no--
-//     if tie--alert and reset
-//     else -- switch player
+});
